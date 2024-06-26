9bb2506dea7c2bfb1df35f81394f597878971b18
tomdan-ai <tomudoh258@gmail.com>
2024-06-26 16:04:15 +0100

    Adjust views to use static files

--- pyblog/views.py
+++ pyblog/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello World!! It Pyblog time.")
+    # return HttpResponse("Hello World!! It Pyblog time.")
+    return render(request, 'home.html')
 
 
 def about(request):
-    return HttpResponse("This is the about Page.")
+    # return HttpResponse("This is the about Page.")
+    return render(request, 'about.html')
